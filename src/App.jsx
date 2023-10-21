bc5ad01f60050528bc78a25a87adc2c38f4f4692
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2023-10-21 03:45:24 +0000

    logic is  added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 import './App.css'
 
 export default function App() {
+  const [add, setAdd] = useState(0);
+
   return (
     <div className="App">
       <div className="container">
         <div className="displaycont">
-          <div className="display">100</div>
+          <div className="display">{add}</div>
         </div>
 
         <div className="btn-container">
-          <button className="btn1">+</button>
-          <button className="btn1">-</button>
+          <button className="btn1" onClick={()=>setAdd(add-1)}>-</button>
+          <button className="btn1" onClick={(e) => setAdd(add + 1)}>+</button>
+          
         </div>
       </div>
     </div>
