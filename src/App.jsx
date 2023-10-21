793d23ba044637d314e4b41f8b6722931b9c3a4b
PratikOdpalliwar <137909115+PratikOdpalliwar@users.noreply.github.com>
2023-10-21 01:30:40 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="displaycont">
+        <div className="display">100</div>
+          </div>
+        
+        <div className="btn-container">
+          <button className="btn1">+</button>
+          <button className="btn2">-</button>
+        </div>
+      </div>
+    </div>
+  )
+}
