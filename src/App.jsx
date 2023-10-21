961b2a23eccccdb1102eb64ce02b2273848fba0e
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2023-10-21 01:36:12 +0000

    Buttons and the display are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import './App.css'
 
 export default function App() {
@@ -5,9 +6,9 @@ export default function App() {
     <div className="App">
       <div className="container">
         <div className="displaycont">
-        <div className="display">100</div>
-          </div>
-        
+          <div className="display">100</div>
+        </div>
+
         <div className="btn-container">
           <button className="btn1">+</button>
           <button className="btn2">-</button>
